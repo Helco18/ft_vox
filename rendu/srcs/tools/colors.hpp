63355777a6a1ca3ed41b8373b108a0d691ad0f05
AgarOther <agarother@hotmail.com>
2025-07-22 19:35:29 +0200

    Added colors

---
+++ rendu/srcs/tools/colors.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   colors.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scraeyme <scraeyme@student.42angouleme.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/30 14:24:47 by scraeyme          #+#    #+#             */
+/*   Updated: 2025/07/22 16:35:31 by scraeyme         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef COLORS_HPP
+# define COLORS_HPP
+# define BLACK "\033[30;49m"
+# define RED "\033[31;49m"
+# define GREEN "\033[32;49m"
+# define YELLOW "\033[33;49m"
+# define BLUE "\033[34;49m"
+# define MAGENTA "\033[35;49m"
+# define CYAN "\033[36;49m"
+# define WHITE "\033[37;49m"
+# define RESET "\033[0m"
+# define BOLD_BLACK "\033[30;1m"
+# define BOLD_RED "\033[31;1m"
+# define BOLD_GREEN "\033[32;1m"
+# define BOLD_YELLOW "\033[33;1m"
+# define BOLD_BLUE "\033[34;1m"
+# define BOLD_MAGENTA "\033[35;1m"
+# define BOLD_CYAN "\033[36;1m"
+# define BOLD_WHITE "\033[37;1m"
+# define BOLD_RESET "\033[0m"
+#endif
