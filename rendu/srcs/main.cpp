d9014e0a3ae3aff6e49fa139924f9d5a8b358d7f
gael <gael>
2025-07-23 16:14:26 +0200

    add icon triangle end stb lib for read png

--- rendu/srcs/main.cpp
+++ rendu/srcs/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: scraeyme <scraeyme@student.42angouleme.    +#+  +:+       +#+        */
+/*   By: gcannaud <gcannaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 14:48:18 by scraeyme          #+#    #+#             */
-/*   Updated: 2025/07/22 19:18:28 by scraeyme         ###   ########.fr       */
+/*   Updated: 2025/07/23 16:07:15 by gcannaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,24 @@
 #include <glm/glm.hpp>
 #include "vulkan/VulkanEngine.hpp"
 #include "colors.hpp"
+#include <GLFW/glfw3.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb/stb_image.h>
+
+GLFWimage decodeOneStep(const char* filename) {
+	GLFWimage image;
+	int width, height, channels;
+	unsigned char* pixels = stbi_load(filename, &width, &height, &channels, STBI_rgb_alpha);
+	if (!pixels) {
+		std::cerr << RED << "[ERROR] Failed to load image: " << filename << RESET << std::endl;
+		exit(1);
+	}
+	image.width = width;
+	image.height = height;
+	image.pixels = pixels;
+
+    return image;
+}
 
 GLFWwindow * getWindow()
 {
@@ -21,7 +39,21 @@ GLFWwindow * getWindow()
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 	GLFWwindow * window = glfwCreateWindow(800, 600, "ft_vox", nullptr, nullptr);
 	if (!window)
+	{
+		std::cerr << RED << "[ERROR] Failed to create GLFW window" << RESET << std::endl;
 		exit(1); // log a faire
+	}
+
+	glfwSetWindowSizeLimits(window, 400, 300, GLFW_DONT_CARE, GLFW_DONT_CARE);
+
+	// Set window icon
+	GLFWimage images[2];
+	images[0] = decodeOneStep("assets/icon.png");
+	images[1] = decodeOneStep("assets/icon_small.png");
+	glfwSetWindowIcon(window, 2, images);
+	// Free the image data after setting the icon
+	stbi_image_free(images[0].pixels);
+	stbi_image_free(images[1].pixels);
 	return (window);
 }
 
